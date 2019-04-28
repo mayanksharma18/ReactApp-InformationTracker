6ad38b05dbefcde67d48709319ef12f4ac52c89f
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-04-29 01:48:38 +0530

    codeChange

--- src/component/ListItems.js
+++ src/component/ListItems.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import Card from '@material-ui/core/Card';
-
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import {Paper,Grid,Avatar,Icon} from '@material-ui/core'
 function List(props){
    let items=props.items
     return(
      <ul>
          
          {
-             items.map((i)=><li><Card>{i.input}{i.time} </Card></li>)
+    //          items.map((i)=><div><li>
+    //              <Card>
+    //   <CardContent>
+    //     <Typography  color="textSecondary" gutterBottom>
+    //       {i.time}
+    //     </Typography>
+    //     <Typography variant="h5" component="h2">
+    //       {i.input}
+    //     </Typography>
+        
+    //   </CardContent>
+    //   </Card>
+    //              </li></div>
+    items.map((i)=>
+        <Paper >
+        <Grid container wrap="nowrap" spacing={16}>
+          <Grid item>
+            <Avatar><Icon>label_important</Icon></Avatar>
+          </Grid>
+          <Grid item xs zeroMinWidth>
+            <Typography noWrap>{i.input}</Typography>
+          </Grid>
+        </Grid>
+        </Paper>
+        )
          }
         
      </ul>
@@ -16,3 +42,4 @@ function List(props){
 
 
 export default List;
+
