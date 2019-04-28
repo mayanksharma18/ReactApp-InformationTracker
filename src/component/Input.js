48f678269a74dd9aa2dc7b3d8cbbeb1dbefca132
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-04-29 02:52:02 +0530

    updates

--- src/component/Input.js
+++ src/component/Input.js
@@ -5,61 +5,58 @@ import List from './ListItems'
 import Icon from '@material-ui/core/Icon';
 import red from '@material-ui/core/colors/red';
 
-export default class InputText extends React.Component{
-        state={
-            input:"", 
-        
-            data:[]
-            
-        }
+export default class InputText extends React.Component {
+    state = {
+        input: "",
 
-    handleChange=(event)=>{
-     this.setState({input:event.target.value})
+        data: []
+
     }
+
+    handleChange = (event) => {
+        this.setState({ input: event.target.value })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        let input = this.state.input;
+
+        let time = new Date().toDateString() + " " + new Date().toLocaleTimeString();
+        console.log("Hi")
+        this.setState({
+            input: "",
+
+            data: [...this.state.data, { input, time }]
+        })
+        localStorage.setItem("data",toString(this.state.data))
     
-    handleSubmit=(event)=>{
-    event.preventDefault();
-
-     let input=this.state.input;
-     
-     let time=new Date().toDateString()+" "+new Date().toLocaleTimeString();
-     console.log("Hi")
-     this.setState({
-         input:"",
-         
-         data:[...this.state.data,{input,time}]
-     })
     }
-       render(){
-           return(
-               <div position="relative">
-            <form onSubmit={this.handleSubmit}>
-        <TextField
-        value={this.state.input}
-         fullWidth="true"
-         margin="normal"
-          label="Name"
-          multiline='true'
-          onChange={this.handleChange}
-          margin="normal"
-          variant="outlined"
-        />
-        <Button  type="submit" variant="contained" color="secondary" >
-       
-        <Icon fontSize="large" >
-        add_circle
-      </Icon> Add you lessons
-      </Button>
-      
-     
-        </form>
- <h1>{this.state.input}</h1>
- 
- <List items={this.state.data}/>
- </div>
+    render() {
+        return (
+            <div position="relative">
+                <form onSubmit={this.handleSubmit}>
+                    <TextField
+                        value={this.state.input}
+                        fullWidth="true"
+                        margin="normal"
+                        label="Name"
+                        multiline='true'
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <Button type="submit" variant="contained" color="secondary" >
+                        <Icon fontSize="large" >add_circle</Icon> Add you lessons
+                   </Button>
+                </form>
+                <h1>{this.state.input}</h1>
 
-           )
-       }
+                <List items={this.state.data} />
+            </div>
+
+        )
+    }
 
 
 }
--- src/component/ListItems.js
+++ src/component/ListItems.js
@@ -1,45 +1,60 @@
-import React from 'react'
+import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import {Paper,Grid,Avatar,Icon} from '@material-ui/core'
-function List(props){
-   let items=props.items
-    return(
-     <ul>
-         
-         {
-    //          items.map((i)=><div><li>
-    //              <Card>
-    //   <CardContent>
-    //     <Typography  color="textSecondary" gutterBottom>
-    //       {i.time}
-    //     </Typography>
-    //     <Typography variant="h5" component="h2">
-    //       {i.input}
-    //     </Typography>
-        
-    //   </CardContent>
-    //   </Card>
-    //              </li></div>
-    items.map((i)=>
-        <Paper >
-        <Grid container wrap="nowrap" spacing={16}>
-          <Grid item>
-            <Avatar><Icon>label_important</Icon></Avatar>
-          </Grid>
-          <Grid item xs zeroMinWidth>
-            <Typography noWrap>{i.input}</Typography>
-          </Grid>
-        </Grid>
-        </Paper>
-        )
-         }
-        
-     </ul>
+import { Paper, Grid, Avatar, Icon, GridList, GridListTile } from '@material-ui/core';
+function List(props) {
+    let items = props.items
+    return (
+        <ol>
+
+            {
+
+                items.map((i) => 
+                <li><div>
+                    <Card>
+                        <CardContent>
+                            <Typography color="textSecondary" gutterBottom>
+                                {i.time}
+                            </Typography>
+                            <Typography variant="h5" component="h2">
+                              <Icon color="primary">label_important</Icon>  {i.input}
+                            </Typography>
+
+                        </CardContent>
+                    </Card>
+                </div></li>)
+            }
+            </ol>
+  
     )
-}
+                }
+
+// items.map((i)=><li>
+// <Card>
+//     <CardContent><b>{i.time}</b></CardContent>
+//     <Paper >
+//     <Grid container wrap="nowrap" spacing={16} >
 
 
-export default List;
+//       <Grid item>
+//         <Avatar><Icon>label_important</Icon></Avatar></Grid>
+//       <Grid  wrap item xs zeroMinWidth alignContent justify>
+//         <Typography variant="h4" noWrap>{i.input}</Typography>
+//         </Grid>
+//       </Grid>
+
+
+
+//     </Paper>
+//     </Card>
+//     </li>
+//     )
+//      }
+
+//  </ol>
+
+
+
+export default List
 
